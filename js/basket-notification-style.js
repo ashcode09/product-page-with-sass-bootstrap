de32d55bacfdd838c87d8aade4656830e4a97a78
Ashleigh Maund <ashleigh.maund@bcsg.com>
2015-11-03 16:25:58 +0000

    amended the logic again for basket update notification sticking to nav bar and top of screen - much shorter and more responsive too

--- js/basket-notification-style.js
+++ js/basket-notification-style.js
@@ -1,28 +1,9 @@
 $(window).scroll(function() {
-	if ($(window).width() < 767) {
-		var heightOfNavBar = $('#navbar').height();
-		var heightOfShoppingBasket = $('#shoppingBasket').height();
-		var bottomOfShoppingBasket = heightOfShoppingBasket + heightOfNavBar;
-		if ($(window).scrollTop() >= bottomOfShoppingBasket) {
-    	$('#basket-update-notn').removeClass('fix-to-navbar-bottom-small-screen fix-to-navbar-bottom');
-		};
-		if ($(window).scrollTop() < bottomOfShoppingBasket) {
-			toggleClasses('#basket-update-notn', 'fix-to-navbar-bottom', 'fix-to-navbar-bottom-small-screen');
-		};
-	} else {
-		var topOfShoppingBasket = $('#shoppingBasket').offset()['top'];
-		var heightOfShoppingBasket = $('#navbar').height();
-		var bottomOfShoppingBasket = topOfShoppingBasket + heightOfShoppingBasket;
-		if ($(window).scrollTop() >= bottomOfShoppingBasket) {
-    	$('#basket-update-notn').removeClass('fix-to-navbar-bottom-small-screen fix-to-navbar-bottom');
-		};
-		if ($(window).scrollTop() < bottomOfShoppingBasket) {
-			toggleClasses('#basket-update-notn', 'fix-to-navbar-bottom-small-screen', 'fix-to-navbar-bottom');
-		};
-	};
+	var bottomOfShoppingBasket = $('#navbar').height() + $('#shoppingBasket').height();
+	if ($(window).scrollTop() >= bottomOfShoppingBasket) {
+		$('#basket-update-notn').addClass('fix-to-top');
+	}
+	if ($(window).scrollTop() < bottomOfShoppingBasket) {
+		$('#basket-update-notn').removeClass('fix-to-top');
+	}
 });
-
-function toggleClasses(idOfElementToStick, classToRemove, classToAdd) {
-	$(idOfElementToStick).removeClass(classToRemove);
-	$(idOfElementToStick).addClass(classToAdd);
-}
